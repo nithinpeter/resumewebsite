b49acd356baf9c25900979b24579fce47869aaf2
Nithin Peter <nithinpeter7@gmail.com>
2015-03-19 01:02:44 +1100

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by nithinpeter on 19/03/15.
+ */
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname+"/public"))
+
+
+app.get('/',function(req,res) {
+    res.send('index.html');
+})
+
+app.listen(process.env.PORT || 9000).listen(3000);
